5dd874e147992c35896f0c3e1325c3bbb9b81e1d
Shashank Dhakate <shashankdkte@gmail.com>
2023-05-07 03:05:26 +0530

    Card Component

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./card-list.styles.css";
+import { Card } from "../card/card.component";
+export const CardList = (props) => {
+  return (
+    <div className="card-list">
+      {props.monster.map((monster) => (
+        <Card monster={monster}></Card>
+      ))}
+    </div>
+  );
+};
